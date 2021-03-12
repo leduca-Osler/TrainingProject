15b0324b05a0ce96aea1b6949cb070517bac39ad
Albert Leduc <leduca@gmail.com>
2021-03-12 09:38:10 -0600

    Added more exceptions to the Minform_KeyDown function.

--- TrainingProject/MainForm.cs
+++ TrainingProject/MainForm.cs
@@ -855,7 +855,8 @@ namespace TrainingApp
 			else if (e.KeyCode == Keys.C) MyGame.clearWarnings();
 			else if (e.KeyCode != Keys.Up && e.KeyCode != Keys.Down && e.KeyCode != Keys.Left 
 				&& e.KeyCode != Keys.Right && e.KeyCode != Keys.LWin && e.KeyCode != Keys.ControlKey 
-				&& e.KeyCode != Keys.Alt && e.KeyCode != Keys.Home && e.KeyCode != Keys.Menu) 
+				&& e.KeyCode != Keys.Alt && e.KeyCode != Keys.Home && e.KeyCode != Keys.Menu
+				&& e.KeyCode != Keys.Tab && e.KeyCode != Keys.MediaPlayPause) 
 				MessageBox.Show("Shortcuts " + e.KeyCode + 
 				"\nPause:               Space" +
 				"\nFight:                Enter / +" +
